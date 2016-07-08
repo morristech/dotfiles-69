4329d22b9860aca5a3aa56cf847c65f2e6b4178d
Laurent Cozic <laurent@cozic.net>
2016-07-08 12:27:05 +0100

    Remove link target if it exists during installation

--- _create_symlinks.sh
+++ _create_symlinks.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+fn_symlink() {
+	local SOURCE="$1"
+	local TARGET="$2"
+
+	# Move target out of the way if it exists.
+	# A bit messy but avoids having to manually
+	# confirm everything, and safer than deleting.
+	if [[ -f "$TARGET" || -d "$TARGET" ]]; then
+		mv -f "$TARGET" "$TARGET.dotfile_bak"
+	fi
+
+	ln -fs "$SOURCE" "$TARGET" 
+}
 
 mkdir -p ~/.config
 
-ln -s "$SCRIPT_DIR/.vim" ~/.vim 
-ln -s ~/.vim/vimrc ~/.vimrc
-
-ln -s "$SCRIPT_DIR/.gitconfig" ~/.gitconfig 
-ln -s "$SCRIPT_DIR/.config/terminator" ~/.config/terminator
+fn_symlink "$SCRIPT_DIR/.vim" ~/.vim
+fn_symlink ~/.vim/vimrc ~/.vimrc
+fn_symlink "$SCRIPT_DIR/.gitconfig" ~/.gitconfig 
+fn_symlink "$SCRIPT_DIR/.config/terminator" ~/.config/terminator
