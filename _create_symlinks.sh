16ac5f009b7cea19927f71e7ae5caad3cbf59295
Laurent Cozic <laurent@cozic.net>
2016-07-08 12:06:11 +0100

    First commit

---
+++ _create_symlinks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+mkdir -p ~/.config
+
+ln -s "$SCRIPT_DIR/.vim" ~/.vim 
+ln -s "$SCRIPT_DIR/.gitconfig" ~/.gitconfig 
+ln -s "$SCRIPT_DIR/.config/terminator" ~/.config/terminator
