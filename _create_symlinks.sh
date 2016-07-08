22342a643e14557909c1a312970e85f387b5aea7
Laurent Cozic <laurent@cozic.net>
2016-07-08 12:11:45 +0100

    Also symlink vimrc

--- _create_symlinks.sh
+++ _create_symlinks.sh
@@ -4,5 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p ~/.config
 
 ln -s "$SCRIPT_DIR/.vim" ~/.vim 
+ln -s ~/.vim/vimrc ~/.vimrc
+
 ln -s "$SCRIPT_DIR/.gitconfig" ~/.gitconfig 
 ln -s "$SCRIPT_DIR/.config/terminator" ~/.config/terminator
