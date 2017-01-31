0eddcef69ca2ab21ff3adf519865ab43fa4e99de
Laurent Cozic <laurent@cohortglobal.com>
2017-01-31 17:33:27 +0000

    Updated ST3 conf

--- _create_symlinks.sh
+++ _create_symlinks.sh
@@ -21,6 +21,7 @@ mkdir -p ~/.config
 fn_symlink "$SCRIPT_DIR/.vim" ~/.vim
 fn_symlink ~/.vim/vimrc ~/.vimrc
 fn_symlink "$SCRIPT_DIR/.gitconfig" ~/.gitconfig 
+fn_symlink "$SCRIPT_DIR/.config/qtcreator" ~/.config/QtProject
 fn_symlink "$SCRIPT_DIR/.config/terminator" ~/.config/terminator
 fn_symlink "$SCRIPT_DIR/.config/sublime-text-3" ~/.config/sublime-text-3
 fn_symlink "$SCRIPT_DIR/.config/SpeedCrunch" ~/.config/SpeedCrunch
